ec6fa2b1f1fccab9afe846388fc77796991ff860
Andrey S <me@9215752443>
2022-11-08 14:13:27 +0300

    Add join fetch

--- src/main/java/ru/kata/spring/boot_security/demo/dao/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserRepository.java
@@ -1,10 +1,19 @@
 package ru.kata.spring.boot_security.demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.kata.spring.boot_security.demo.model.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String name);
 
-
+    @Query("select user from User user join fetch user.roles where user.username = :username")
+    User findByUsername(@Param("username") String username);
 }
+
+
+
+
+
+
